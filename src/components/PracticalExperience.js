34e13bfd92c80afc1eb663d0b50de395437db6cb
Brian Mariscal Rodriguez <briannish@gmail.com>
2022-09-13 19:28:03 -0400

    Update PracticalExperience component to be a function instead of a class

--- src/components/PracticalExperience.js
+++ src/components/PracticalExperience.js
@@ -1,97 +1,86 @@
-import { Component } from "react";
+import { useState } from "react";
 
-class PracticalExperience extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: this.props.data,
-      mode: this.props.mode
-    };
-  }
+const PracticalExperience = (props) => {
+  const [data, setData] = useState(props.data);
+  const [mode, setMode] = useState(props.mode);
 
-  sendData = (e) => {
+  const sendData = (e) => {
     e.preventDefault();
-    this.props.updatePracticalExperience(this.state.data, 'practicalExperience');
-    this.setState({
-      mode: 'show'
-    });
+    props.updatePracticalExperience(data, 'practicalExperience');
+    setMode('show');
   };
 
-  handleChange = (e) => {
+  const handleChange = (e) => {
     const { dataset: { property }, value } = e.currentTarget;
-    this.setState({
-      data: {
-        ...this.state.data,
-        [property]: value
-      }
+    setData({
+      ...data,
+      [property]: value
     });
   };
 
-  render() {
-    const { data: { id, companyName, position, title, mainTasks, dateFrom, dateUntil }, mode } = this.state;
-    if (mode === 'edit') {
-      return (<div className="segment">
-        <form onSubmit={this.sendData}>
+  const { id, companyName, position, title, mainTasks, dateFrom, dateUntil } = data;
+  if (mode === 'edit') {
+    return (<div className="segment">
+      <form onSubmit={sendData}>
+      <div className="field">
+          <label>Company name: </label>
+          <input type="text" data-property="companyName" value={companyName} onChange={handleChange} required/>
+        </div>
         <div className="field">
-            <label>Company name: </label>
-            <input type="text" data-property="companyName" value={companyName} onChange={this.handleChange} required/>
-          </div>
-          <div className="field">
-            <label>Position: </label>
-            <input type="text" data-property="position" value={position} onChange={this.handleChange} required/>
-          </div>
-          <div className="field">
-            <label>Title: </label>
-            <input type="text" data-property="title" value={title} onChange={this.handleChange} required/>
-          </div>
-          <div className="field">
-            <label>Main tasks: </label>
-            <textarea data-property="mainTasks" value={mainTasks} onChange={this.handleChange} cols="50" required>
-            </textarea>
-          </div>
-          <div className="field">
-            <label>From: </label>
-            <input type="date" data-property="dateFrom" value={dateFrom} onChange={this.handleChange} required/>
-          </div>
-          <div className="field">
-            <label>Until: </label>
-            <input type="date" data-property="dateUntil" value={dateUntil} onChange={this.handleChange}/>
-          </div>
-          <div className="buttons">
-            <button type="submit">Save</button>
-            <button type="button" onClick={() => this.setState({ mode: 'show' })}>Cancel</button>
-          </div>
-        </form>
-      </div>);
-    }
-    if (mode === 'show') {
-      return (<div className="segment">
-        <div className="show field">
-          <label>Company Name: </label>
-          <span>{companyName}</span>
-        </div>
-        <div className="show field">
           <label>Position: </label>
-          <span>{position}</span>
+          <input type="text" data-property="position" value={position} onChange={handleChange} required/>
         </div>
-        <div className="show field">
+        <div className="field">
           <label>Title: </label>
-          <span>{title}</span>
+          <input type="text" data-property="title" value={title} onChange={handleChange} required/>
         </div>
-        <div className="show field">
+        <div className="field">
           <label>Main tasks: </label>
-          <span>{mainTasks}</span>
+          <textarea data-property="mainTasks" value={mainTasks} onChange={handleChange} cols="50" required>
+          </textarea>
         </div>
-        <div className="show field">
-          <label>Date: </label>
-          <span>{dateFrom} - {dateUntil ? dateUntil : 'current'}</span>
+        <div className="field">
+          <label>From: </label>
+          <input type="date" data-property="dateFrom" value={dateFrom} onChange={handleChange} required/>
+        </div>
+        <div className="field">
+          <label>Until: </label>
+          <input type="date" data-property="dateUntil" value={dateUntil} onChange={handleChange}/>
         </div>
         <div className="buttons">
-            <button type="button" onClick={() => this.setState({ mode: 'edit' })}>Edit</button>
-            <button type="button" onClick={() => this.props.removeExperience({ id, type: 'practicalExperience' })}>Remove</button>
+          <button type="submit">Save</button>
+          <button type="button" onClick={() => setMode('show')}>Cancel</button>
         </div>
-      </div>);
-    }
+      </form>
+    </div>);
+  }
+  if (mode === 'show') {
+    return (<div className="segment">
+      <div className="show field">
+        <label>Company Name: </label>
+        <span>{companyName}</span>
+      </div>
+      <div className="show field">
+        <label>Position: </label>
+        <span>{position}</span>
+      </div>
+      <div className="show field">
+        <label>Title: </label>
+        <span>{title}</span>
+      </div>
+      <div className="show field">
+        <label>Main tasks: </label>
+        <span>{mainTasks}</span>
+      </div>
+      <div className="show field">
+        <label>Date: </label>
+        <span>{dateFrom} - {dateUntil ? dateUntil : 'current'}</span>
+      </div>
+      <div className="buttons">
+          <button type="button" onClick={() => setMode('edit')}>Edit</button>
+          <button type="button" onClick={() => props.removeExperience({ id, type: 'practicalExperience' })}>Remove</button>
+      </div>
+    </div>);
   }
 }
 
