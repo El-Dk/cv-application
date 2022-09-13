82b431ee226006e72ce6657757143defdfb68f36
Brian Mariscal Rodriguez <briannish@gmail.com>
2022-09-13 19:13:23 -0400

    Update GeneralInformation component to be a function instead of a class

--- src/components/GeneralInformation.js
+++ src/components/GeneralInformation.js
@@ -1,85 +1,74 @@
-import { Component } from "react";
+import { useState } from "react";
 
-class GeneralInformation extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: this.props.data,
-      mode: this.props.mode
-    };
-  }
+const GeneralInformation = (props) => {
+  const [data, setData] = useState(props.data);
+  const [mode, setMode] = useState(props.mode);
 
-  sendData = (e) => {
+  const sendData = (e) => {
     e.preventDefault();
-    const { address, age, birthDate, fullName } = this.state.data;
-    this.props.updateGeneralInformation({ address, age, birthDate, fullName });
-    this.setState({
-      mode: 'show'
-    });
+    const { address, age, birthDate, fullName } = data;
+    props.updateGeneralInformation({ address, age, birthDate, fullName });
+    setMode('show');
   };
 
-  handleChange = (e) => {
+  const handleChange = (e) => {
     const { dataset: { property }, value } = e.currentTarget;
-    this.setState({
-      data: {
-        ...this.state.data,
-        [property]: value
-      }
+    setData({
+      ...data,
+      [property]: value
     });
   };
 
-  render() {
-    const { address, age, birthDate, fullName } = this.state.data;
-    if (this.state.mode === 'edit') {
-      return(<div className="container">
-        <h2>General Information</h2>
-        <form onSubmit={this.sendData}>
-          <div className="field">
-            <label>Full Name: </label>
-            <input type="text" data-property="fullName" value={fullName} onChange={this.handleChange} required/>
-          </div>
-          <div className="field">
-            <label>Age: </label>
-            <input type="number" data-property="age" value={age} onChange={this.handleChange} required/>
-          </div>
-          <div className="field">
-            <label>Address: </label>
-            <input type="text" data-property="address" value={address} onChange={this.handleChange} required/>
-          </div>
-          <div className="field">
-            <label>Birthdate: </label>
-            <input type="date" data-property="birthDate" value={birthDate} onChange={this.handleChange} required/>
-          </div>
-          <div className="buttons">
-            <button type="submit">Save</button>
-            <button type="button" onClick={() => this.setState({ mode: 'show' })}>Cancel</button>
-          </div>
-        </form>
-      </div>);
-    }
-    if (this.state.mode ===  'show') {
-      return (<div className="container">
-        <div className="show field">
+  const { address, age, birthDate, fullName } = data;
+  if (mode === 'edit') {
+    return(<div className="container">
+      <h2>General Information</h2>
+      <form onSubmit={sendData}>
+        <div className="field">
           <label>Full Name: </label>
-          <span>{fullName}</span>
+          <input type="text" data-property="fullName" value={fullName} onChange={handleChange} required/>
         </div>
-        <div className="show field">
+        <div className="field">
           <label>Age: </label>
-          <span>{age}</span>
+          <input type="number" data-property="age" value={age} onChange={handleChange} required/>
         </div>
-        <div className="show field">
+        <div className="field">
           <label>Address: </label>
-          <span>{address}</span>
+          <input type="text" data-property="address" value={address} onChange={handleChange} required/>
         </div>
-        <div className="show field">
+        <div className="field">
           <label>Birthdate: </label>
-          <span>{birthDate}</span>
+          <input type="date" data-property="birthDate" value={birthDate} onChange={handleChange} required/>
         </div>
         <div className="buttons">
-            <button type="button" onClick={() => this.setState({ mode: 'edit' })}>Edit</button>
+          <button type="submit">Save</button>
+          <button type="button" onClick={() => setMode('show')}>Cancel</button>
         </div>
-      </div>);
-    }
+      </form>
+    </div>);
+  }
+  if (mode ===  'show') {
+    return (<div className="container">
+      <div className="show field">
+        <label>Full Name: </label>
+        <span>{fullName}</span>
+      </div>
+      <div className="show field">
+        <label>Age: </label>
+        <span>{age}</span>
+      </div>
+      <div className="show field">
+        <label>Address: </label>
+        <span>{address}</span>
+      </div>
+      <div className="show field">
+        <label>Birthdate: </label>
+        <span>{birthDate}</span>
+      </div>
+      <div className="buttons">
+          <button type="button" onClick={() => setMode('edit')}>Edit</button>
+      </div>
+    </div>);
   }
 }
 
